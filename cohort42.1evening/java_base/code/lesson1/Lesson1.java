85b574b25a4a33827c971f33c5f7395f99cf5a53
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2024-08-27 22:10:06 +0200

    add lesson01jb

---
+++ cohort42.1evening/java_base/code/lesson1/Lesson1.java
@@ -0,0 +1,13 @@
+package lesson1;
+/**
+ * AIT-TR, Cohort 42.1, Java Basic, Lesson #1
+ *
+ * @author Valerian
+ * @version 16-Jan-2024
+ */
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
