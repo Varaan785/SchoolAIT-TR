41f72dd787ad5a8c8c292bff845476401927e42b
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2024-08-28 21:48:58 +0200

    Create HomeWork1.java

---
+++ cohort42.1evening/java_base/code/lesson1/homeWork1/HomeWork1.java
@@ -0,0 +1,29 @@
+package lesson1.homeWork1;
+/**
+ * AIT-TR, Cohoirt 42.1, Java Basic, HW #1
+ *
+ * @author Valerian
+ * @version 31.-Jan-24
+ */
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+
+        //ask #1
+        System.out.println("task #1");
+        System.out.println("Rady");
+
+        //task #2
+        System.out.println("task #2");
+        System.out.println("< > + - , . : ; ( ) { } /  "); // \
+        System.out.println("Rady");
+
+        //task #3
+        System.out.println("task #3");
+        System.out.println("My name is Valerian");
+        System.out.println("I was born in the Year of the Ox");
+        System.out.println("I am from Earth");
+        System.out.println("Rady");
+
+    }
+}
