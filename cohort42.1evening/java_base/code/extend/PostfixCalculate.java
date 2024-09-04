ce83012a52e699c76f0d8b6712fdd3043caedb37
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2024-09-04 22:06:42 +0200

    add extend

---
+++ cohort42.1evening/java_base/code/extend/PostfixCalculate.java
@@ -0,0 +1,14 @@
+package extend;
+
+import java.util.Stack;
+
+public class PostfixCalculate {
+    public static void main(String[] args) {
+        String[] postfix = {"10","5","+"};
+        Stack<String> stack = new Stack<>();
+
+        for (String str : postfix) {
+
+        }
+    }
+}
